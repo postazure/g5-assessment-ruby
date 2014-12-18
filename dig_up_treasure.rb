e1c5a43c3a33ecf0c932355eba3fafa85a5f198f
Max Gordon <gordonmaxc@gmail.com>
2014-12-18 11:21:31 -0800

    Improved

---
+++ dig_up_treasure.rb
@@ -0,0 +1,51 @@
+require_relative "./lib/poem_charter"
+
+puts PoemCharter.new("./data/*").run
+
+# Expected Output
+# {
+#   "William Butler Yeats"=>{
+#     "A Drinking Song"=>{
+#       :verses=>1,
+#       :lines=>6
+#     },
+#     "A Cradle Song"=>{
+#       :verses=>1,
+#       :lines=>12
+#     }
+#   },
+#   "Walt Whitman"=>{
+#     "Aboard At A Ship's Helm"=>{
+#       :verses=>5,
+#       :lines=>11
+#     }
+#   },
+#   "Robert Lee Frost"=>{
+#     "The Lockless Door"=>{
+#       :verses=>5,
+#       :lines=>20
+#     },
+#     "A Brook In The City"=>{
+#       :verses=>1,
+#       :lines=>9
+#     },
+#     "A Cliff Dwelling"=>{
+#       :verses=>1,
+#       :lines=>13
+#     }
+#   },
+#   "Henry Lawson"=>{
+#     "As far As Your Rifles Cover"=>{
+#       :verses=>2,
+#       :lines=>8
+#     },
+#     "I'll Tell You What You Wanderers"=>{
+#       :verses=>1,
+#       :lines=>8
+#     },
+#     "In Possum Land"=>{
+#       :verses=>2,
+#       :lines=>8
+#     }
+#   }
+# }
--- lib/poem_charter.rb
+++ lib/poem_charter.rb
@@ -1,4 +1,62 @@
 class PoemCharter
-  
+  def initialize(dir = "../data/*")
+    @dir = dir
+    @poem_hash = {}
+  end
 
+  def run
+    file_names.each do |file_name|
+      poem_stats = get_poem_stats(file_name)
+      log_poem(poem_stats)
+    end
+
+    @poem_hash
+  end
+
+  def file_names
+    Dir[@dir]
+  end
+
+  def get_poem_stats(file_name)
+    poem_info = {
+      title: false,
+      author: false,
+      line_count: 0,
+      verse_count: 0,
+    }
+
+    f = File.open(file_name, "r")
+    f.each_line do |line|
+
+      if poem_info[:title] == false
+        poem_info[:title] = line.chomp
+        next
+      elsif poem_info[:author] == false
+        poem_info[:author] = line.chomp
+        next
+      elsif line == "\n"
+        poem_info[:verse_count] += 1
+        next
+      end
+
+      poem_info[:line_count] += 1
+    end
+
+    poem_info
+  end
+
+  def log_poem(poem_stats)
+    poem_data = {
+      poem_stats[:title] => {
+        verses: poem_stats[:verse_count],
+        lines: poem_stats[:line_count],
+      }
+    }
+
+    if @poem_hash.has_key?(poem_stats[:author])
+      @poem_hash[poem_stats[:author]].merge!(poem_data)
+    else
+      @poem_hash.merge!({poem_stats[:author] => poem_data})
+    end
+  end
 end
